b074f9f59063caffb01f578e1338e51df2b75c0c
Mario Arturo Lopez Martinez <mario.lopez.arturo@gmail.com>
2020-08-28 19:59:27 -0500

    Updating packages and contacts.

--- src/views/Sections/TeamSection.js
+++ src/views/Sections/TeamSection.js
@@ -4,12 +4,8 @@
 
 import React from "react";
 
-import Billicarole from "assets/img/profile/Billicarole.jpeg";
 import Booth from "assets/img/profile/Booth.jpg";
-import Brandon from "assets/img/profile/Brandon.jpeg";
 import Donahoo from "assets/img/profile/Donahoo.jpg";
-import Neil from "assets/img/profile/Neil.png";
-import Mario from "assets/img/profile/Mario.jpeg";
 import Meghan from "assets/img/profile/Meghan.jpg";
 import Miranda from "assets/img/profile/Miranda.jpeg";
 import DefaultAvatar from "assets/img/default-avatar.png";
@@ -27,19 +23,20 @@ function TeamSection() {
             <Col md="4">
               <div className="team-player">
                 <img
-                  alt="Mario Profile"
+                  alt="Meghan Bibb"
                   className="rounded-circle img-fluid img-raised"
-                  src={Mario}
+                  src={Meghan}
                 />
-                <h4 className="title">Mario Lopez</h4>
+                <h4 className="title">Meghan Bibb</h4>
                 <p className="category text-info">President</p>
                 <p>Computer Science Senior</p>
                 <p>Baylor University</p>
                 <Button
                   className="btn-icon btn-round"
                   color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
+                  href="https://www.linkedin.com/in/meghanbibb/"
+                  target="_blank"
+                  onClick={(e) => e.preventDefault()}
                 >
                   <i className="fab fa-linkedin" />
                 </Button>
@@ -48,103 +45,20 @@ function TeamSection() {
             <Col md="4">
               <div className="team-player">
                 <img
-                  alt="Brandon Profile"
+                  alt="Brooklynn Profile"
                   className="rounded-circle img-fluid img-raised"
-                  src={Brandon}
+                  src={DefaultAvatar}
                 />
-                <h4 className="title">Brandon Mork</h4>
+                <h4 className="title">Brooklynn Stone</h4>
                 <p className="category text-info">Vice President</p>
-                <p>Computer Science Senior</p>
+                <p>Computer Science Sophomore</p>
                 <p>Baylor University</p>
                 <Button
                   className="btn-icon btn-round"
                   color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="fab fa-linkedin" />
-                </Button>
-              </div>
-            </Col>
-            <Col md="4">
-              <div className="team-player">
-                <img
-                  alt="Billicarole Profile"
-                  className="rounded-circle img-fluid img-raised"
-                  src={Billicarole}
-                />
-                <h4 className="title">Billicarole Evans</h4>
-                <p className="category text-info">Treasurer</p>
-                <p>Mechanical Engineering Senior</p>
-                <p>Baylor University</p>
-                <Button
-                  className="btn-icon btn-round"
-                  color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="fab fa-linkedin" />
-                </Button>
-              </div>
-            </Col>
-            <Col md="4">
-              <div className="team-player">
-                <img
-                  alt="..."
-                  className="rounded-circle img-fluid img-raised"
-                  src={Meghan}
-                />
-                <h4 className="title">Megan Bibb</h4>
-                <p className="category text-info">Head of Philanthropy</p>
-                <p>Computer Science Junior</p>
-                <p>Baylor University</p>
-                <Button
-                  className="btn-icon btn-round"
-                  color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="fab fa-linkedin" />
-                </Button>
-              </div>
-            </Col>
-            <Col md="4">
-              <div className="team-player">
-                <img
-                  alt="..."
-                  className="rounded-circle img-fluid img-raised"
-                  src={Neil}
-                />
-                <h4 className="title">Neil Wilcoxen</h4>
-                <p className="category text-info">Secretary</p>
-                <p>Computer Science Senior</p>
-                <p>Baylor University</p>
-                <Button
-                  className="btn-icon btn-round"
-                  color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="fab fa-linkedin" />
-                </Button>
-              </div>
-            </Col>
-            <Col md="4">
-              <div className="team-player">
-                <img
-                  alt="Miranda Profile"
-                  className="rounded-circle img-fluid img-raised"
-                  src={Miranda}
-                />
-                <h4 className="title">Miranda Montroy</h4>
-                <p className="category text-info">External Engagement Coordinator</p>
-                <p>Management Information Systems Sophomore</p>
-                <p>Baylor University</p>
-                <Button
-                  className="btn-icon btn-round"
-                  color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
+                  href="https://www.linkedin.com/in/brooklynn-stone/"
+                  target="_blank"
+                  onClick={(e) => e.preventDefault()}
                 >
                   <i className="fab fa-linkedin" />
                 </Button>
@@ -158,17 +72,75 @@ function TeamSection() {
                   src={DefaultAvatar}
                 />
                 <h4 className="title">Citlally Ruiz</h4>
-                <p className="category text-info">Marketing Chair</p>
-                <p>Computer Science Freshman</p>
+                <p className="category text-info">Treasurer</p>
+                <p>Computer Science Sophomore</p>
                 <p>Baylor University</p>
                 <Button
                   className="btn-icon btn-round"
                   color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
+                  href="https://www.linkedin.com/in/citlally-ruiz-29429119a/"
+                  target="_blank"
+                  onClick={(e) => e.preventDefault()}
                 >
                   <i className="fab fa-linkedin" />
                 </Button>
+              </div>
+            </Col>
+            <Col md="4">
+              <div className="team-player">
+                <img
+                  alt="Miranda Profile"
+                  className="rounded-circle img-fluid img-raised"
+                  src={Miranda}
+                />
+                <h4 className="title">Miranda Montroy</h4>
+                <p className="category text-info">IT Projects Manager</p>
+                <p>Management Information Systems Junior</p>
+                <p>Baylor University</p>
+                <Button
+                  className="btn-icon btn-round"
+                  color="info"
+                  href="https://www.linkedin.com/in/miranda-montroy-78107117b/"
+                  target="_blank"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <i className="fab fa-linkedin" />
+                </Button>
+              </div>
+            </Col>
+            <Col md="4">
+              <div className="team-player">
+                <img
+                  alt="Ashley Profile"
+                  className="rounded-circle img-fluid img-raised"
+                  src={DefaultAvatar}
+                />
+                <h4 className="title">Ashley Lu Couch</h4>
+                <p className="category text-info">Head of Philanthropy</p>
+                <p>Computer Science Junior</p>
+                <p>Baylor University</p>
+                <Button
+                  className="btn-icon btn-round"
+                  color="info"
+                  href="https://www.linkedin.com/in/ashleyluellen/"
+                  target="_blank"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <i className="fab fa-linkedin" />
+                </Button>
+              </div>
+            </Col>
+            <Col md="4">
+              <div className="team-player">
+                <img
+                  alt="Andrew Profile"
+                  className="rounded-circle img-fluid img-raised"
+                  src={DefaultAvatar}
+                />
+                <h4 className="title">Andrew Ammentorp</h4>
+                <p className="category text-info">Secretary</p>
+                <p>Computer Science Junior</p>
+                <p>Baylor University</p>
               </div>
             </Col>
             <Col md="4">
@@ -182,14 +154,6 @@ function TeamSection() {
                 <p className="category text-info">Advisor, Co-Founder</p>
                 <p>Professor of Computer Science</p>
                 <p>Baylor University</p>
-                <Button
-                  className="btn-icon btn-round"
-                  color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="fab fa-linkedin" />
-                </Button>
               </div>
             </Col>
             <Col md="4">
@@ -203,14 +167,6 @@ function TeamSection() {
                 <p className="category text-info">Advisor, Co-Founder</p>
                 <p>Senior Lecturer of Computer Science</p>
                 <p>Baylor University</p>
-                <Button
-                  className="btn-icon btn-round"
-                  color="info"
-                  href="#pablo"
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="fab fa-linkedin" />
-                </Button>
               </div>
             </Col>
           </Row>
